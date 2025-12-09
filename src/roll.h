5f3ce6f619587dbf87c21b16846f5052fbace186
graybeasley <grayton.beasley@g.austincc.edu>
2025-12-09 00:29:47 +0000

    Question 2

--- src/roll.h
+++ src/roll.h
@@ -1,1 +1,18 @@
-//
+#ifndef ROLL_H
+#define ROLL_H
+
+#include "die.h"
+
+class Roll {
+public:
+    Roll(Die& d1, Die& d2);
+    void roll_dice();
+    int roll_value() const;
+
+private:
+    Die& die1;
+    Die& die2;
+    int rolled_value;
+};
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -14,5 +15,18 @@ TEST_CASE("Die roll returns a value from 1 to 6", "[Die]") {
         int val = die.roll();
         REQUIRE(val >= 1);
         REQUIRE(val <= 6);
+    }
+}
+
+TEST_CASE("Roll class returns values from 2 to 12", "[Roll]") {
+    srand(time(0));
+    Die die1, die2;
+    Roll roll(die1, die2);
+
+    for(int i = 0; i < 10; i++) {
+        roll.roll_dice();
+        int val = roll.roll_value();
+        REQUIRE(val >= 2);
+        REQUIRE(val <= 12);
     }
 }
